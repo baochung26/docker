01d2ba5d8df25559202b6c2d3ec32866e18439e5
chungpb <phungbaochung@gmail.com>
2020-03-15 22:11:39 +0700

    fix

--- public_html/index.php
+++ public_html/index.php
@@ -2,10 +2,10 @@
 <h4>Attempting MySQL connection from php...</h4>
 <?php 
 $host = 'mysql';
-$user = 'root';
-$pass = 'secret';
+$user = 'testuser';
+$pass = 'testpass';
 $db = 'test_db';
-$conn = new mysqli($host, $user, $pass);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
